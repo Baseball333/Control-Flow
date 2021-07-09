f7bdf81dfb5db2746436a528cadda798d2f3ec5d
Baseball333 <52107974+Baseball333@users.noreply.github.com>
2021-07-08 22:03:01 -0700

    Write foundsearch.c

---
+++ foundsearch.c
@@ -0,0 +1,9 @@
+found = 0;
+for (i = 0; i < n && !found; i++)
+  for (j = 0; j < n && !found j++)
+    if (a[i] == b[j])
+         found = 1;
+    if (found)
+      /* Got one: a[i-1] == b[j] */
+    else
+      /* Didn't find any common elements */
