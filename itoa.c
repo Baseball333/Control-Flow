9cdd110787de428e7ffedc78cfc37b504a53b29d
Baseball333 <52107974+Baseball333@users.noreply.github.com>
2021-07-23 22:23:41 -0700

    Update itoa.c

--- itoa.c
+++ itoa.c
@@ -1,1 +1,8 @@
-
+/* Itoa: Convert n to characters in s */
+void itoa(int n, char s[])
+{
+  int i, sign;
+  
+  if ((sign = n) < 0) /* Record sign variable */
+    n = -n; /* Make n positive */
+  
