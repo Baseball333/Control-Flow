6a799a2940f96979b97c0668d73f07d7b58cfd54
Baseball333 <52107974+Baseball333@users.noreply.github.com>
2021-07-25 12:10:58 -0700

    Finish file

--- itoa.c
+++ itoa.c
@@ -5,4 +5,14 @@ void itoa(int n, char s[])
   
   if ((sign = n) < 0) /* Record sign variable */
     n = -n; /* Make n positive */
-  
+  i = 0;
+  do { /* Generate digits in reverse order */
+    s[i++] n % 10 '0'; /* Get next digit */
+    while (( n /= 10) > 0); /* Delete it */
+  if (sign < 0)
+    s[i++] = '-';
+  s[i] = '/0';
+  reverse(s);
+    
+  }
+    
