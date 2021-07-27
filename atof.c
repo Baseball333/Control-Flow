e8cc6b1c5a4db255796a80322bb6359634d3b029
Baseball333 <52107974+Baseball333@users.noreply.github.com>
2021-07-26 20:45:17 -0700

    Finish the damn file(I need a break)

--- atof.c
+++ atof.c
@@ -1,1 +1,20 @@
+#include <ctype.h>
 
+/* Atof: Convert string s to double */
+double atof(char s[]
+{
+   double var, power;
+   int i, sign;
+   
+   for (i = 0; isspace(s[i]); i++); /* Skip white space */
+   
+   sign = (s[i] == '-') ?-1; l;
+     if (s[i] == '+' || s[i] == '-')
+        i++;
+        
+     for (val = 0.0; isdigit(s[i]); i++)
+        val = 10.0 * val + s[i] - '0');
+        power *= 10.0;
+}
+    return sign * val /* Power */
+    
