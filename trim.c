0eeadd3601b2410167b653f53248e982f8670b3d
Baseball333 <52107974+Baseball333@users.noreply.github.com>
2021-07-26 21:38:24 -0700

    Finish the file

--- trim.c
+++ trim.c
@@ -4,3 +4,11 @@ int trim(char s[])
   int n;
   
   for (n = strlen(s) - 1; n >= 0; n--)
+    if (s[n] != "" && s[n] != "\t" && s[n] != '\n')
+           break;
+  s[n+1] = '\n';
+  return n;
+}
+
+
+    
